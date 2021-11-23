baf11203cff716857b5c4d2a9299839aa734e9ca
Vinayak Singh <vinayaksingh5302@gmail.com>
2021-11-24 01:15:24 +0530

    added buttons for info switch

--- src/components/repo-details/index.js
+++ src/components/repo-details/index.js
@@ -1,15 +1,44 @@
-import React,{ useContext } from 'react'
+import React,{ useContext, useState, useEffect } from 'react'
 import RepositoryContext from '../../contexts/repositoryList'
 
 function RepoDetails({data}) {
     // if(data)
     //     console.log(data);
-    const {repoList, setRepoList } = useContext(RepositoryContext);
+    const { setRepoList } = useContext(RepositoryContext);
+    const [category, setCategory] = useState('branches');
+    const [catData, setCatData] = useState();
 
-    return (
+    useEffect(()=>{
+        fetch(`https://api.github.com/repos/${data.owner.login}/${data.name}/branches`)
+            .then(response => response.json())
+            .then(data =>
+                setCatData(JSON.stringify(data))
+                // JSON.stringify(data)
+                //console.log(data);
+            ).catch(e => console.log(e));
+    },[data]);
+    
+    useEffect(()=>{
+
+        document.getElementById(category).classList.add('active');
+    },[category]);
+
+
+    const handleCategoryChange =(c) =>{
+        // console.log(`https://api.github.com/repos/${data.owner.login}/${data.name}/${category}`)
+        fetch(`https://api.github.com/repos/${data.owner.login}/${data.name}/${c}`)
+        .then(response => response.json())
+        .then(data => 
+            setCatData(JSON.stringify(data))
+            // JSON.stringify(data)
+            //console.log(data);
+        ).catch(e => console.log(e));
+    }
+
+    return data ? (
         <div className="right--pane">
             <div className='top--row'>
-                {data && <p><img src={data.owner.avatar_url} alt=''/>{data.name}</p>}
+                <p><img src={data.owner.avatar_url} alt=''/>{data.name}</p>
                 <button className='delete--button' onClick={() => {
                     setRepoList(ogList => ogList.filter(repo => repo.id !== data.id))
                 }}>
@@ -17,10 +46,36 @@ function RepoDetails({data}) {
                 </button>
             </div>
             <div className='detail-section'>
-                <p>Hello</p>
+                <div className='view--buttons'>
+                    <p className='list--button active'id='branches' onClick={() => {
+                        if(!document.getElementById(`branches`).classList.contains('active'))
+                            {
+                            document.getElementById(category).classList.toggle('active')
+                                setCategory('branches');
+                                handleCategoryChange('branches');
+                            }
+                    }}>Branches</p>
+                    <p className='list--button' id='issues' onClick={() => {
+                        if (!document.getElementById(`issues`).classList.contains('active')) {
+                            document.getElementById(category).classList.toggle('active')
+                            setCategory('issues');
+                            handleCategoryChange('issues');
+                        }
+                    }}>Issues</p>
+                    <p className='list--button' id='commits' onClick={() => {
+                        if (!document.getElementById(`commits`).classList.contains('active')) {
+                            document.getElementById(category).classList.toggle('active')
+                            setCategory('commits');
+                            handleCategoryChange('commits');
+                        }
+                    }}>Commits</p>
+                </div>
+                <div className='repo--detail'>
+                    {catData}
+                </div>
             </div>
         </div>
-    );
+    ): null;
 }
 
 export default RepoDetails;
